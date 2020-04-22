c96127f8d3b8099965088faa0e65b53c3c33a2a4
InKyoJeong <viapolar@naver.com>
2020-04-22 21:41:31 +0900

    test: Handle routes

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ const PORT = 4000;
 function handleListening() {
   console.log(`Listening on : http://localhost:${PORT}`);
 }
+
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hello from Home");
+}
+
+function handelProfile(req, res) {
+  res.send("Your Pofile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handelProfile);
+
 app.listen(PORT, handleListening);

7f7448a59764f4001974c6b981dea1ca34416010
InKyoJeong <viapolar@naver.com>
2020-04-26 22:10:32 +0900

    chore: Install babel and Set nodemon

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on : http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
-  console.log(req);
-  res.send("Hello from Home");
-}
+const handleHome = (req, res) => res.send("Hello from Home");
 
-function handelProfile(req, res) {
-  res.send("Your Pofile");
-}
+const handelProfile = (req, res) => res.send("Your Pofile");
 
 app.get("/", handleHome);
 

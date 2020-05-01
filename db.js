7cbd67e5bed8f02618342e38f2bd7ffb0a3d682a
InKyoJeong <viapolar@naver.com>
2020-05-02 01:31:49 +0900

    docs: Add mixin with pug

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Inkyo",
+      email: "inkyo.dev@gmail.com",
+    },
+  },
+  {
+    id: 12312123,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Inkyo",
+      email: "inkyo.dev@gmail.com",
+    },
+  },
+  {
+    id: 444444,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Inkyo",
+      email: "inkyo.dev@gmail.com",
+    },
+  },
+  {
+    id: 22221,
+    title: "Video something",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Inkyo",
+      email: "inkyo.dev@gmail.com",
+    },
+  },
+];
